b3f6f6775d39f971c58f35c4ae96981d0c5d1b62
Rakshit Maggon <rakshitmaggon70@gmail.com>
2020-05-02 17:09:02 +0530

    Commit

--- stack.c
+++ stack.c
@@ -1,9 +1,8 @@
-//STACK -POP , PUSH , DISPLAY 
 #include<stdio.h>
 int main()
 {
     int num,i,size,var1,top=-1,repeat,j;
-    printf("enter the size of your stack:");
+    printf("Enter the size of your stack:");
     scanf("%d",&size);
     int stack[size];
     do
@@ -37,26 +36,23 @@ int main()
             }
             else
             {
-                printf("The top most element of stack is %d",stack[top]);
+                printf("The top most element of stack is : %d \n",stack[top]);
                 top--;
             }
             break;
         
             case 3:
-            if(top==!-1)
-            {
-                printf("stack values are going to be displayed");
+                printf("stack values are going to be displayed\n");
                 for(i=top;i>=0;i--)
                 {
                     printf("%d",stack[i]);
                 }
-            }
+                break;
+            
         }    
-        printf("do you want to repeat the process??(1-yes,0-no):");
+        printf("\nDo you want to repeat the process??(1-yes,0-no):");
         scanf("%d",&j);
     } while(j==1);
     
     return 0;
 }
-
-
